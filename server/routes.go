a68d8e9fd1ffe7022705c6fcf594e368bb0eb77f
Chris Beaver <chris@chrisbeaver.net>
2018-08-11 13:45:07 -0400

    Proof of concept is working, pin 10 toggles LED.

--- server/routes.go
+++ server/routes.go
@@ -4,8 +4,16 @@ import (
 	// "encoding/json"
     // "log"
 	"net/http"
+	"time"
+	"os"
+	"fmt"
 	// "io"
 	"github.com/stianeikeland/go-rpio"
+)
+
+var (
+	// Use mcu pin 10, corresponds to physical pin 19 on the pi
+	pin = rpio.Pin(10)
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,15 +25,23 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	// In the future we could report back on the status of our DB, or our cache
 	// (e.g. Redis) by performing a simple PING, and include them in the response.
 	// io.WriteString(w, `{"alive": true}`)
-	pin := rpio.Pin(10)
+	// Open and map memory to access gpio, check for errors
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	pin.Output()       // Output mode
-	pin.High()         // Set pin High
-	pin.Low()          // Set pin Low
-	pin.Toggle()       // Toggle pin (Low -> High -> Low)
+	// Unmap gpio memory when done
+	defer rpio.Close()
 
-	pin.Input()        // Input mode
-	// io.WriteString(w, pin.Read())  // Read state from pin (High / Low)
+	// Set pin to output mode
+	pin.Output()
+
+	// Toggle pin 20 times
+	for x := 0; x < 20; x++ {
+		pin.Toggle()
+		time.Sleep(time.Second / 5)
+	}
 }
 
 
